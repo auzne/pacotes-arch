9f17fb398a51c68dc48a58db96d13019e6d91470
vinicius <viniciusbmarco10@gmail.com>
2022-03-22 18:06:21 -0300

    gdm abria o gnome, vamo de lightdm mesmo

--- xfce.sh
+++ xfce.sh
@@ -4,10 +4,10 @@
 # O pacote sudo é necessário
 
 # instala os pacotes
-sudo pacman -Syu --needed --noconfirm xorg xorg-server xfce4 xfce4-goodies gdm
+sudo pacman -Syu --needed --noconfirm xorg xorg-server xfce4 xfce4-goodies lightdm
 # coloca como padrão graphical.target caso o usuário tenha desativado
 sudo systemctl set-default graphical.target
-# ativa o gdm
-sudo systemctl enable gdm
+# ativa o lightdm
+sudo systemctl enable lightdm
 # reinicia o sistema
 sudo restart
